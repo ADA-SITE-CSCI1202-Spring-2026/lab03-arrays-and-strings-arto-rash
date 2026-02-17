93816548227541aff8bab605a4fa2e5b5e81038c
rashid.artoghrul <rashid.artoghrul@gmail.com>
2026-02-17 10:18:45 +0400

    initial commit - reverse function implemented

---
+++ StringUtil.java
@@ -0,0 +1,12 @@
+package Lab3;
+
+public class StringUtil {
+
+  public static String reverse(String s) {
+    StringBuilder sb = new StringBuilder();
+    for (int i=s.length()-1;i>=0;i--) {
+      sb.append(s.charAt(i));
+    }
+    return sb.toString();
+  }
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+package Lab3;
+
+public class Test {
+  public static void main(String[] args) {
+    String a = "Hello";
+    System.out.println(StringUtil.reverse(a));
+  }
+}
